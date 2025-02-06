b72b165aa26ee8522f816f38f5b574574a57571f
Likith P <p.likith1999@gmail.com>
2025-02-06 14:05:15 -0500

    4 patterns

---
+++ revABC.rs
@@ -0,0 +1,14 @@
+use std::io;
+fn main(){
+    let mut input = String::new();
+    println!("Enter the no. of rows:");
+    io::stdin().read_line(&mut input).expect("failed");
+    let number: usize = input.trim().parse().expect("please type a number");
+    let ch = 'A';
+    for i in (0..number).rev() {
+        for j in 0..=i {
+            print!("{}",(ch as u8 + j as u8) as char);
+        }
+        println!("");
+    }
+}
