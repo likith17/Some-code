5e433869b91624cd90ba803fcca5344a57c17de3
Likith P <p.likith1999@gmail.com>
2025-02-13 01:15:21 -0500

    diamond on my own

--- diamond.rs
+++ diamond.rs
@@ -1,10 +1,26 @@
 use std::io;
 fn main(){
     let mut input = String::new();
-    println("Enter the no. of rows");
+    println!("Enter the no. of rows");
     io::stdin().read_line(&mut input).expect("input fail");
     let n: usize = input.trim().parse().expect("only number");
     for i in 0..n {
-        
+    for _ in 0..(n-i-1){
+        print!(" ");
     }
+    for _ in 0..(2*i+1){
+        print!("*");
+    }
+    println!("");
+}       
+    for j in 1..n{
+        for _ in 0..j{
+            print!(" ");
+        }
+        for _ in 0..(2*(n-j)-1){
+            print!("*");
+        }
+        println!("");
+    }
+    
 }
--- equiTriangle.rs
+++ equiTriangle.rs
@@ -11,11 +11,9 @@ let n: usize = match input.trim().parse(){
         return;
     }
 };
-for i in 0..n {
+for i in 0..n { 
 for _ in 0..(n-i-1) {
     print!(" ");
-    
-    
 }
 
 for _ in 0..(2*i+1) {
